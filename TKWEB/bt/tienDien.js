972e51a331490ad04578dc6c98c0f5707177a25a
quynhquynh154 <nguyenquynh170820@gmail.com>
2022-09-04 11:16:30 +0700

    addfile

---
+++ TKWEB/bt/tienDien.js
@@ -0,0 +1,6 @@
+function myFunc() {
+  let name = document.getElementById("name").value;
+  let oldNumber = document.getElementById("oldNumber").value;
+  let newNumber = document.getElementById("newNumber").value;
+  let unitPrice = document.getElementById("unitPrice").value;
+}
